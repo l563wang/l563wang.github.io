92240de25696aebba7cdedcf2f4bd2a906e6ffc5
l563wang <l563wang@uwaterloo.ca>
2020-10-07 19:23:12 -0400

    Updates

---
+++ precache-manifest.86cad51e452cba18b222fdac9bfe64c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c3c8a24e7b2921416a092dbaddb846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad633949cd5c4c022c97",
+    "url": "/static/css/main.c0710856.chunk.css"
+  },
+  {
+    "revision": "f5c045bc057141453b5e",
+    "url": "/static/js/2.60093287.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/static/js/2.60093287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad633949cd5c4c022c97",
+    "url": "/static/js/main.22a7be98.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5881845414da318a98e718046704607e",
+    "url": "/static/media/profile.58818454.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/l563wang.github.io/precache-manifest.24be245b2d310739b730892073025fd4.js"
+  "/precache-manifest.86cad51e452cba18b222fdac9bfe64c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/l563wang.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
